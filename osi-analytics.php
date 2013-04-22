8630e1fea3ed6e736662e0974147bf4f91b34b8d
AJ Foster <web@aj-foster.com>
2013-04-21 21:47:50 -0400

    Modify indentation to match MiniBill theme.

--- osi-analytics.php
+++ osi-analytics.php
@@ -45,18 +45,18 @@ function analytics() {
 	$option = get_option('osi-analytics');
 
 	if ($option['enable'] == "true") {
-		?>
-			<script type="text/javascript">
-				var _gaq = _gaq || [];
-				_gaq.push(['_setAccount', '<?php analytics_code(); ?>']);
-				_gaq.push(['_trackPageview']);
-				(function() {
-					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-				})();
-			</script>
-		<?php
+?>
+<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '<?php analytics_code(); ?>']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+<?php
 	}
 }
 add_action('wp_footer', 'analytics');
